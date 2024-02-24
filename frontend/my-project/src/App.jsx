c0c063964238270034eaa7ddbee183f0cc0a3e12
Arsalanbashir831 <arsalanbashir831@gmail.com>
2024-02-24 15:22:07 +0500

    profile

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
     <>
-      <Signup/>
+      <Profile/>
     </>
   )
 }
