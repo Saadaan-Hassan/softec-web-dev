160d8cbe919195651578f8e4b1a1044a2761c25e
Saadaan Hassan <webmaker9d@gmail.com>
2024-02-25 02:51:05 +0500

    intigrate contact-us form

--- frontend/my-project/src/App.jsx
+++ frontend/my-project/src/App.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import Profile from './pages/Profile'
-import Community from './pages/Community'
-import Home from './pages/Home'
-import BlogPage from './pages/BlogPage'
-import AddPost from './pages/AddPost'
-import HajjUmmrahGuide from './pages/HajjUmmrahGuide'
-import ContactUs from './pages/ContactUs'
+import React from "react";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Community from "./pages/Community";
+import Home from "./pages/Home";
+import BlogPage from "./pages/BlogPage";
+import AddPost from "./pages/AddPost";
+import HajjUmmrahGuide from "./pages/HajjUmmrahGuide";
+import ContactUs from "./pages/ContactUs";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.withCredentials = true;
 
 const App = () => {
-  return (
-    <>
-      {/* <Community/> */}
-      {/* <BlogPage/> */}
-      {/* <AddPost/> */}
-      {/* <HajjUmmrahGuide/> */}
-      <ContactUs/>
-    </>
-  )
-}
+	return (
+		<>
+			{/* <Community/> */}
+			{/* <BlogPage/> */}
+			{/* <AddPost/> */}
+			{/* <HajjUmmrahGuide/> */}
+			<ContactUs />
+		</>
+	);
+};
 
-export default App
+export default App;
