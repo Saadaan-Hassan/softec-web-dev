32705c0ec5bb49efa1491a52aec2d8bc59bda755
Shafin zaman <shafinzaman42@gmail.com>
2024-02-24 19:53:33 +0500

    Auth added

--- backend/src/routes/queries.routes.js
+++ backend/src/routes/queries.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import {  addQuery, queryAns, getQuery,getQueries } from "../controllers/queries.controller.js";
+import { addQuery, queryAns, getQuery, getQueries } from "../controllers/queries.controller.js";
 import { checkAdminAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/", getQueries);
+router.get("/", checkAdminAuth, getQueries);
 router.get("/:id", getQuery);
-router.post("/", addQuery);
-router.patch("/:id", queryAns);
+router.post("/", checkAdminAuth, addQuery);
+router.patch("/:id", checkAdminAuth, queryAns);
 
 export default router;
